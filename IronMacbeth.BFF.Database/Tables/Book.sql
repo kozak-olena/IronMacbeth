11ccc6dab93353a5b2c851db0d975a00a8a7e558
Olena Kozak <kozak.olena11@gmail.com>
2020-11-05 18:16:36 +0200

    create bookDB with attributes

--- IronMacbeth.BFF.Database/Tables/Book.sql
+++ IronMacbeth.BFF.Database/Tables/Book.sql
@@ -1,5 +1,14 @@
 ﻿CREATE TABLE [dbo].[Book]
 (
-    [Id] INT NOT NULL PRIMARY KEY
+    [Id] VARCHAR (255) NOT NULL PRIMARY KEY,
+    [BookName] VARCHAR (255) NOT NULL,
+    [BookAuthor] VARCHAR (255) NOT NULL,
+    [PublishingHouse] VARCHAR (255) NOT NULL,
+    [City] VARCHAR (255) NOT NULL,
+    [PublicationYear] VARCHAR (255) NOT NULL,
+    [NumberOfPages] VARCHAR (255) NOT NULL,
+    [TypeOfDocument] VARCHAR (255) NOT NULL,
+    [Availability] VARCHAR (255) NOT NULL,
+    [Location] VARCHAR (255) NOT NULL,
 
 )
