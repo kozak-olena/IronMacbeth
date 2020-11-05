b0941f0e65788cadf93872b2cbd5f814f1fe7fc7
Olena Kozak <kozak.olena11@gmail.com>
2020-11-05 21:38:23 +0200

    added rating and comment to Article, Periodical, added electronic version to Book and Thesis

--- IronMacbeth.BFF.Database/Tables/Article.sql
+++ IronMacbeth.BFF.Database/Tables/Article.sql
@@ -6,5 +6,8 @@
     [PublicationYear] VARCHAR (255) NOT NULL,
     [NumberOfPages] VARCHAR (255) NOT NULL,
     [TypeOfDocument] VARCHAR (255) NOT NULL,
-    [MainDocumentId] INT NOT NULL FOREIGN KEY REFERENCES Book(BookId)
+    [MainDocumentId] INT NOT NULL FOREIGN KEY REFERENCES Book(BookId),
+    [ElectronicVersion]VARCHAR (255) NOT NULL,
+    [Rating]VARCHAR (255) NOT NULL,
+    [Comments]VARCHAR (255) NOT NULL,
 )
--- IronMacbeth.BFF.Database/Tables/Periodical.sql
+++ IronMacbeth.BFF.Database/Tables/Periodical.sql
@@ -3,7 +3,7 @@
     [PeriodicalId] INT NOT NULL PRIMARY KEY,
     [NameOfPeriodical] VARCHAR (255) NOT NULL,
     [ResponsibleAuthors] VARCHAR (255) NOT NULL,
-     [IssueNumber]VARCHAR (255) NOT NULL,
+    [IssueNumber]VARCHAR (255) NOT NULL,
     [PublishingHouse] VARCHAR (255) NOT NULL,
     [City] VARCHAR (255) NOT NULL,
     [Year] VARCHAR (255) NOT NULL,
@@ -12,5 +12,6 @@
     [Location]VARCHAR (255) NOT NULL,
     [TypeOfDocument]VARCHAR (255) NOT NULL,
     [ElectronicVersion]VARCHAR (255) NOT NULL,
-    
+    [Rating]VARCHAR (255) NOT NULL,
+    [Comments]VARCHAR (255) NOT NULL,
 )
