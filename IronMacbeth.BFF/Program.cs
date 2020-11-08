0cbad20595b8349eaaf46f6c0d4f3a7b47afd98a
Oleksandr Vinnyk <ovinnyk@deloitte.com>
2020-11-08 23:51:37 +0200

    add identitymodel stub, fix login via enter button press issue

--- IronMacbeth.BFF/Program.cs
+++ IronMacbeth.BFF/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IdentityModel.Configuration;
 using System.ServiceModel;
 
 namespace IronMacbeth.BFF
@@ -10,6 +11,14 @@ namespace IronMacbeth.BFF
             using (var host = new ServiceHost(typeof (Service)))          //Конструкция using оформляет блок кода и создает объект некоторого класса, который реализует интерфейс IDisposable,
                 //в частности, его метод Dispose. При завершении блока кода у объекта вызывается метод Dispose.
             {
+                host.Credentials.UseIdentityConfiguration = true;
+
+                var idConfig = new IdentityConfiguration();
+
+                idConfig.SecurityTokenHandlers.AddOrReplace(new UserNameSecurityTokenHandler());
+
+                host.Credentials.IdentityConfiguration = idConfig;
+
                 Console.WriteLine("Starting Server...");
 
                 try
