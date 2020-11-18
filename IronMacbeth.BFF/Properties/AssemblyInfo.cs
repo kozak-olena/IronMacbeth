f3b71489578f4a908cbc0f5615d78a510e8c3deb
Oleksandr Vinnyk <ovinnyk@deloitte.com>
2020-11-18 20:57:54 +0200

    update assembly infos

--- IronMacbeth.BFF/Properties/AssemblyInfo.cs
+++ IronMacbeth.BFF/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("WCFService")]
+[assembly: AssemblyTitle("IronMacbeth.BFF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WCFService")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("IronMacbeth.BFF")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
